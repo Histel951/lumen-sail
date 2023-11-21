3355b1b121ac00055ec439fde99ab38e5a67dcfa
Histel951 <histel951@gmail.com>
2023-11-21 15:00:09 +0300

    fix errors

--- src/Maker/Docker/V2/DockerYmlVolumesMaker.php
+++ src/Maker/Docker/V2/DockerYmlVolumesMaker.php
@@ -5,6 +5,7 @@ namespace Histel\LumenSail\Maker\Docker\V2;
 
 use Histel\LumenSail\DockerServicesEnum as DSE;
 use Histel\LumenSail\Maker\Docker\AbstractDockerMaker;
+use Illuminate\Support\Collection;
 
 class DockerYmlVolumesMaker extends AbstractDockerMaker
 {
@@ -17,9 +18,9 @@ class DockerYmlVolumesMaker extends AbstractDockerMaker
         DSE::MEILI_SEARCH,
     ];
 
-    public function make(array $services = [], array $config = []): array
+    public function make(array $services = [], array $config = null): array
     {
-        $config = collect($services)
+        collect($services)
             ->filter(function ($service) {
                 return in_array($service, $this->usesServices);
             })->filter(function ($service) use ($config) {
